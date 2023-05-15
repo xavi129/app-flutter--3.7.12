ffe7462cf004d8b88a0141b7a5943f1fa5366bfe
xavi129 <xavi33541@gmail.com>
2023-05-14 22:01:00 -0700

    add built to ignore

--- build/app/generated/source/buildConfig/release/com/SQ/Entregas/BuildConfig.java
+++ build/app/generated/source/buildConfig/release/com/SQ/Entregas/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.SQ.Entregas";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 77;
-  public static final String VERSION_NAME = "1.71.77";
+  public static final int VERSION_CODE = 21;
+  public static final String VERSION_NAME = "0.0.21";
 }
